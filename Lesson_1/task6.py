f4fee73cdf686e91eccafe2177adf4d9803d56c1
Dmitry Serov <eitelkeit999@gmail.com>
2022-05-25 22:52:20 +0400

    fix

--- Lesson_1/task6.py
+++ Lesson_1/task6.py
@@ -1,11 +1,10 @@
-words = ["сетевое программирование", "сокет", "декоратор"]
-f_n = open("test.txt", "w")
-for word in words:
-    f_n.write(f"{word}\n")
+import chardet
+
+f_n = open("test_file.txt", "rb")
+print(chardet.detect(f_n.read())['encoding'])
 f_n.close()
-print(f_n.encoding)
 
 
-f_n = open("test.txt", "r", encoding='UTF-8')
+f_n = open("test_file.txt", "r", encoding='UTF-8', errors='replace')
 print(f_n.read())
 f_n.close()
