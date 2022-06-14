0ae725fed021d9adec7990267eb0e0052eea1e65
Dmitry Serov <eitelkeit999@gmail.com>
2022-06-14 05:51:54 +0400

    gitignore

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 from socket import *
 
-from common.utils import *
+from GB_pyhton_CS.common.utils import *
 
 
-def form_presence(username, status="Just connected"):
+def presence_constructor(username='guest', status="Just connected"):
     presence = {"action": "presence",
                 "type": "status",
                 "user": {
@@ -13,7 +13,7 @@ def form_presence(username, status="Just connected"):
     return presence
 
 
-def form_message(to="guest", from_="guest", message="Hello world"):
+def message_constructor(to="guest", from_="guest", message="Hello world"):
     message = {
         "action": "msg",
         "to": to,
@@ -29,67 +29,71 @@ def response_handler(response):
         print('success')
         return response['payload']
     else:
-        raise Exception(f'unknown error: code {status}')
+        raise ValueError(f'unknown error: code {status}')
 
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect(get_address())
-
-username = input('Enter your username: ')
-msg = form_presence(username)
-s.send(wrap(msg))
-response = s.recv(1024)
-response = unwrap(response)
-response_handler(response)
-s.close()
-
-# Presence established, run CLI interface
-while True:
-    command = input('type your command:\n')
-    command = command.split(' ')
-    command_type = command[0].strip()
-
-    s = create_connection(get_address())
-
-    if command_type == 'presence':
-        request = form_presence(username)
-        request = json.dumps(request).encode()
-        s.send(request)
-        response = s.recv(1024)
-        response = json.loads(response)
-        print(response)
-
-    elif command_type == 'send':
-        to = command[1]
-        message = " ".join(command[2:])
-        request = form_message(to, from_=username, message=message)
-        request = wrap(request)
-        s.send(request)
-        response = s.recv(1024)
-        response = unwrap(response)
-        response_handler(response)
-
-    elif command_type == 'read':
-        request = {
-            "action": "get_msgs",
-            "user": {
-                "username": username
-            },
-        }
-        request = wrap(request)
-        s.send(request)
-        response = s.recv(1024)
-        response = unwrap(response)
-        messages = response_handler(response)
-        if len(messages) > 0:
-            for msg in messages:
-                print(f'New message from {msg["from"]}:\n {msg["message"]}')
-
-    elif command_type == 'exit':
-        s.close()
-        break
-
-    else:
-        print('unknown command')
-
+if __name__ == '__main__':
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(get_address())
+    username = input('Enter your username: ')
+    msg = presence_constructor(username)
+    s.send(wrap(msg))
+    response = s.recv(1024)
+    response = unwrap(response)
+    response_handler(response)
     s.close()
+
+    # Presence established, run CLI interface
+    while True:
+        command = input('type your command:\n')
+        command = command.split(' ')
+        command_type = command[0].strip()
+
+        s = create_connection(get_address())
+
+        # Simple CLI:
+        # 'presence' establishes or renews client's presence on server
+        # 'send user message' - send message to user
+        # 'read' - print unread messages
+        if command_type == 'presence':
+            request = presence_constructor(username)
+            request = json.dumps(request).encode()
+            s.send(request)
+            response = s.recv(1024)
+            response = json.loads(response)
+            print(response)
+
+        elif command_type == 'send':
+            to = command[1]
+            message = " ".join(command[2:])
+            request = message_constructor(to, from_=username, message=message)
+            request = wrap(request)
+            s.send(request)
+            response = s.recv(1024)
+            response = unwrap(response)
+            response_handler(response)
+
+        elif command_type == 'read':
+            request = {
+                "action": "get_msgs",
+                "user": {
+                    "username": username
+                },
+            }
+            request = wrap(request)
+            s.send(request)
+            response = s.recv(1024)
+            response = unwrap(response)
+            messages = response_handler(response)
+            if len(messages) > 0:
+                for msg in messages:
+                    print(f'New message from {msg["from"]}:\n {msg["message"]}')
+
+        elif command_type == 'exit':
+            s.close()
+            break
+
+        else:
+            print('unknown command')
+
+        s.close()
