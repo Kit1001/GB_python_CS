aad900d18dfcf64a52f555a76c68bd6507ed4a0c
Dmitry Serov <eitelkeit999@gmail.com>
2022-06-16 23:03:50 +0400

    client logger saving to file

--- log/client_log_config.py
+++ log/client_log_config.py
@@ -1,13 +1,25 @@
 import logging
+import os
 import sys
 
 log = logging.getLogger('client')
 log.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stderr)
+
+
+stream_handler = logging.StreamHandler(sys.stderr)
 format_ = logging.Formatter('%(levelname)-10s %(asctime)-30s %(module)-25s %(message)-20s')
-handler.setFormatter(format_)
-handler.setLevel(logging.DEBUG)
-log.addHandler(handler)
+stream_handler.setFormatter(format_)
+stream_handler.setLevel(logging.DEBUG)
+log.addHandler(stream_handler)
+
+path = os.path.join(os.path.dirname(__file__), "logs")
+path = os.path.join(path, "client")
+path = os.path.join(path, "app.log")
+file_handler = logging.FileHandler(path)
+file_handler.setFormatter(format_)
+file_handler.setLevel(logging.WARNING)
+log.addHandler(file_handler)
+
 
 if __name__ == '__main__':
     log.debug('debug')
--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import logging
 import log.client_log_config
 
 
-logger = logging.getLogger('client')
-logger.debug('hello')
+log = logging.getLogger('client')
+log.critical('fubar')
